6ff6fb341119c49a2ca37163efdcee4d89b4cd63
Julian <ajarizaga@gmail.com>
2020-10-29 22:08:23 -0300

    agregadas las tablas de ciudades y provincias

---
+++ database/migrations/2020_10_30_010601_add_foreign_key_to_cities.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeyToCities extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cities', function (Blueprint $table) {
+            $table->foreignId('id_province')->references('id')->on('provinces');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cities', function (Blueprint $table) {
+            //
+        });
+    }
+}
